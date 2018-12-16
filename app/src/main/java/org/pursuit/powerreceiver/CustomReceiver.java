fdafb2a7a348766149240007ca155914f82317d6
marliedee <marlyd364@gmail.com>
2018-12-16 17:15:17 -0500

    first commit

---
+++ app/src/main/java/org/pursuit/powerreceiver/CustomReceiver.java
@@ -0,0 +1,36 @@
+package org.pursuit.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import static org.pursuit.powerreceiver.MainActivity.ACTION_CUSTOM_BROADCAST;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if (intentAction != null) {
+            String toastMessage = "unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected!";
+                    break;
+                case Intent.ACTION_HEADSET_PLUG:
+                    toastMessage = "Headset Connected!";
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broadcast Received";
+                    break;
+            }
+
+            Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+}
